fafd5085babb94c252da73a5a4a799ff24ff381e
Nandakishorpalei <66058183+Nandakishorpalei@users.noreply.github.com>
2020-09-23 17:06:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var boy=prompt("what is your 👦 name ?");
 var girl=prompt("what is your pertners 👧 name ?");
 var total=Math.floor(Math.random()*40)+60;
-  document.querySelectorAll("h1")[0].innerHTML="Your Name is " +boy.slice(0,1).toUpperCase()+boy.slice(1,boy.length).toLowerCase();
-    document.querySelectorAll("h1")[1].innerHTML="Your Partners Name is " +girl.slice(0,1).toUpperCase()+girl.slice(1,girl.length).toLowerCase();
+var boyname=boy.slice(0,1).toUpperCase()+boy.slice(1,boy.length).toLowerCase();
+var girlname=girl.slice(0,1).toUpperCase()+girl.slice(1,girl.length).toLowerCase();
+  document.querySelectorAll("h1")[0].innerHTML="Your Name is " +boyname;
+    document.querySelectorAll("h1")[1].innerHTML="Your Partners Name is " +girlname;
 if(total<=79){
-  document.querySelectorAll("h1")[2].innerHTML=boy+" and " +girl + " your ❤ 🥰  percentage  is " +total +"%";
+  document.querySelectorAll("h1")[2].innerHTML=boyname+" and " +girlname + " your ❤ 🥰  percentage  is " +total +"%";
 }
 else{
-  document.querySelectorAll("h1")[2].innerHTML="wowwwww "+boy+" and " +girl +  " your ❤ 🥰 percentage is " +total +"%";
+  document.querySelectorAll("h1")[2].innerHTML="wowwwww "+boyname+ " and " + girlname +  " your ❤ 🥰 percentage is " +total +"%";
 }
